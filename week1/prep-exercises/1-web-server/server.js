63927a4a017ec39c1f90697ed2e0b0155487ae39
DESKTOP-1N0FNIS\elias <eliasrajha7@gmail.com>
2024-09-21 22:58:31 +0200

    prep-exercises week1

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -1,14 +1,31 @@
-/**
- * Exercise 3: Create an HTTP web server
- */
-
 const http = require('http');
+const fs = require('fs').promises; 
 
-//create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+let server = http.createServer(async function (req, res) {
+  try {
+    if (req.url === '/') {
+      const html = await fs.readFile('index.html');
+      res.setHeader('Content-Type', 'text/html');
+      res.write(html);
+    } else if (req.url === '/index.js') {
+      const js = await fs.readFile('index.js');
+      res.setHeader('Content-Type', 'application/javascript');
+      res.write(js);
+    } else if (req.url === '/style.css') {
+      const css = await fs.readFile('style.css');
+      res.setHeader('Content-Type', 'text/css');
+      res.write(css);
+    } else {
+      res.statusCode = 404;
+      res.write('404: Not Found');
+    }
+  } catch (error) {
+    res.statusCode = 500;
+    res.write('Server error');
+  }
+  res.end(); 
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+});
