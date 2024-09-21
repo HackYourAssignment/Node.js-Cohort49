f1b2efb000c6cdee96c93437dd690b6136706052
Alia Taher <alia.taher022@gmail.com>
2024-09-21 22:35:24 +0200

    prep-exercise week 1

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,11 +3,33 @@
  */
 
 const http = require('http');
+const { readFile } = require('fs/promises');
+
 
 //create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
+let server = http.createServer(async function (req, res) {
+	
+	try{
+		const htmlFile = await readFile('index.html');
+		if(req.url === `/`){
+			res.setHeader('Content-Type', 'text/html')
+			res.write(htmlFile);
+		}
+		else if(req.url === `/index.js`){
+			const jsFile = await readFile('index.js')
+			res.setHeader('Content-Type', 'text/javascript')
+			res.write(jsFile);
+		}
+		else if(req.url === '/style.css'){
+		const cssFile = await readFile('style.css')
+		res.setHeader('Content-Type','text/css')
+		res.write(cssFile);
+		}
+	}catch(error){
+		console.log(error)
+		res.write('Error loading the page');
+	}
+	
 	res.end(); // Ends the response
 });
 
