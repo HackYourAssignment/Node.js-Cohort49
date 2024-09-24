9a2b3733266529f2a91c442bedffa2899e09a367
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-09-24 21:37:48 +0200

    practice exercise

--- week1/practice-exercises/2-left-pad/script.js
+++ week1/practice-exercises/2-left-pad/script.js
@@ -1,13 +1,16 @@
 /**
  ** Exercise 2: To the left, to the left...
- * 
- * Copy and paste your code from the previous exercise.
- * Replace the function `padLeft` to use
- * this new NPM package called `left-pad` instead then
- * Pad the numbers to 8 characters to confirm that it works correctly
+ *
+ * In this file use the left-pad package to
+ * pad the numbers to 8 characters and log them to the console
  *
  */
 
-let numbers = [ "12", "846", "2", "1236" ];
+const leftPad = require("left-pad");
 
-// YOUR CODE GOES HERE
+let numbers = ["12", "846", "2", "1236"];
+
+// Pad each number to a length of 8 using spaces and log it
+numbers.forEach((number) => {
+  console.log(leftPad(number, 8, " "));
+});
