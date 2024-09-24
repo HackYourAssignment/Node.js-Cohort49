67862b0728e48b75e7467b623aae79d37900f975
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-09-24 21:39:23 +0200

    rest of the practice exercise

--- week1/practice-exercises/1-pad-numbers/script.js
+++ week1/practice-exercises/1-pad-numbers/script.js
@@ -1,21 +1,25 @@
-
 /**
- ** Exercise 1: Pad numbers 
- * 
+ ** Exercise 1: Pad numbers
+ *
  * In this file use the padLeft function from padLeft.js to
  * pad the numbers to exactly 5 spaces and log them to the console
- * 
+ *
  * Expected output (replace the underscore with spaces):
- * 
+ *
  *  ___12;
  *  __846;
  *  ____2;
  *  _1236;
- * 
+ *
  * Tips:
  *   where to use `exports` and where `require`?
  */
 
-let numbers = [ "12", "846", "2", "1236" ];
+const padLeft = require("./padLeft");
 
-// YOUR CODE GOES HERE
+let numbers = ["12", "846", "2", "1236"];
+
+// Pad each number to a length of 5 using spaces and log it
+numbers.forEach((number) => {
+  console.log(padLeft(number, 5, " "));
+});
