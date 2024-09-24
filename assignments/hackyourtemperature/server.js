430111258dc5e7d3df54d0ad4feaa33260565137
Alia Taher <alia.taher022@gmail.com>
2024-09-24 14:34:35 +0200

    complete assignment

---
+++ assignments/hackyourtemperature/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello from backend to frontend!");
+});
+app.use(express.json());
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(cityName);
+});
+
+app.listen(3000);
