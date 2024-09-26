019608ae7be7958a363aaad16a6c6b80e5abaa29
bteicho@gmail.com <btegicho@gmail.com>
2024-09-26 23:13:27 +0200

    update based on the feedbacks

--- assignments/hackyourtemperature/server.js
+++ assignments/hackyourtemperature/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.post('/weather', (req, res) => {
   const cityName = req.body.cityName;
   if (cityName) {
-    res.Status(200).json({ message: 'received city: ${cityName}' });
+    res.status(200).json({ message: 'received city: ${cityName}' });
   } else {
     res.status(400).json({ error: 'cityName is required in the request body' });
   }
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
 app.listen(port, () => {
-  console.log(`server running o port ${port}`);
+  console.log(`server running on port ${port}`);
 });
