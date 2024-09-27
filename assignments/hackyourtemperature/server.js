b6ec7c47f88be3e32486007f035f41eac48ed9d6
DESKTOP-1N0FNIS\elias <eliasrajha7@gmail.com>
2024-09-27 02:06:00 +0200

    Moved unrelated files to a new branch and removed those commits from the PR. Fixed indentation to make the code clearer. Moved app.use(express.json()); to be grouped logically with other setup code.

---
+++ assignments/hackyourtemperature/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());  // Moved this line up
+
+// Set server to listen on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('hello from backend to frontend!');
+});
+
+app.post('/weather', (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(`You requested the weather for: ${cityName}`);
+});
